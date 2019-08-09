9ae47137d670dde2187c024b44bc04768bd5d128
Stefan Hagen <stefan@ikbenfrits.nl>
2019-08-09 14:41:21 +0200

    Removed opening and closing from pig.enter_barn

--- app/models/animals/chicken.rb
+++ app/models/animals/chicken.rb
@@ -1,6 +1,7 @@
 module Animals
   class Chicken < ApplicationRecord
     prepend Machines::NewChip
+
     def enter_barn
       puts "#{name} enters the barn."
     end
--- app/models/animals/pig.rb
+++ app/models/animals/pig.rb
@@ -1,11 +1,7 @@
 module Animals
   class Pig < ApplicationRecord
     def enter_barn
-      Buildings::Barn.open_door
-
       puts "#{name} enters the barn."
-
-      Buildings::Barn.close_door
     end
   end
 end
